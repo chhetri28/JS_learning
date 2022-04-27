633a2aee7411ea8ef7425e3652e85bed31085945
chhetri28 <chettrisourav28@gmail.com>
2022-04-27 12:43:01 +0530

    Added  the rest of the methods

--- Js_Linked-list/linked_list.js
+++ Js_Linked-list/linked_list.js
@@ -49,15 +49,82 @@ class LinkedList {
       }
       return -1;
   }
+
+  getElementAt(index) {
+    if (index < 0 || index > this.size()) {
+      return undefined;
+    }
+
+    let current = this.head;
+    for (let i = 0; i < index && current !== null; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  removeAt(index){
+    if (index < 0 || index > this.size()) {
+        return undefined;
+    }
+    let current=this.head;
+    if(index===0){
+        this.head=current.next;
+    }else{
+        const previous=this.getElementAt(index-1);
+        current=previous.next;
+        previous.next=current.next;
+    }
+    this.count--;
+    return current.element;
+  }
   remove(element){
       const index =this.indexOf(element);
       return this.removeAt(index);
   }
+  insertAt(element,index){
+    if (index < 0 || index > this.size()) {
+        return undefined;
+      }
+  
+      const node = new Node(element);
+      if (index === 0) {
+        const current = this.head;
+        node.next = current;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        node.next = current;
+        previous.next = node;
+      }
+  
+      this.count++;
+      return node;
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const linkedListArr = [];
+    let current = this.head;
+    for (let i = 0; i < this.size() && current !== null; i++) {
+      linkedListArr.push(current.element);
+      current = current.next;
+    }
+
+    return linkedListArr.join(', ');
+  }
   size() {
     return this.count;
   }
+
   isEmpty() {
     return this.size === 0;
+  }
+
+  clear() {
+    this.head = null;
+    this.count = 0;
   }
 }
 
