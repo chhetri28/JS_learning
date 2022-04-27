cf1f0ea35256ce75f9e376d8e353db8b76dd41ee
chhetri28 <chettrisourav28@gmail.com>
2022-04-27 10:34:46 +0530

    main file

--- Js_Linked-list/main.js
+++ Js_Linked-list/main.js
@@ -1,4 +1,12 @@
+import LinkedList from "./linked_list.js";
+import renderLinkedList from './render.js';
+import initiateHandlers from './ui_handlers.js';
+
+
+const linkedlist = new LinkedList();
 const main = () => {
-    console.log("Hello");
+    console.log("hello")
+    initiateHandlers(linkedlist,renderLinkedList);
+    renderLinkedList(linkedlist);
 };
 main();
