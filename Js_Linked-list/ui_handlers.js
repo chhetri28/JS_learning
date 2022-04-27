1471afe1ae041d78ce2e119ed449100643464c71
chhetri28 <chettrisourav28@gmail.com>
2022-04-27 12:43:40 +0530

    implemented rest of UI handler buttons

--- Js_Linked-list/ui_handlers.js
+++ Js_Linked-list/ui_handlers.js
@@ -25,6 +25,34 @@ const initiateHandlers=(linkedlist,renderLinkedList)=>{
             alert('Element not found');
         }
     });
+    insertAtBtn.addEventListener('click',()=>{
+        const element = prompt('Enter element to add to linkedlist');
+        const index = prompt('Enter the index the element is to be added at');
+        linkedlist.insertAt(element, Number(index));
+        renderLinkedList(linkedlist);
+    });
+    removeElementBtn.addEventListener('click',()=>{
+        const element=prompt('Enter the element to remove from linkedlist');
+        const removedE1=linkedlist.remove(element);
+        if (removedE1) {
+            alert('Element removed');
+            renderLinkedList(linkedlist);
+          } else {
+            alert('Element not found');
+        }
+    });
+    getElementAtBtn.addEventListener('click', () => {
+        const index = prompt('Enter the index the element is to be retrieved from');
+        const node = linkedlist.getElementAt(Number(index));
+        if (node) {
+          alert(`Element retrieved = ${node.element}`);
+        } else {
+          alert('Element not found');
+        }
+      });
+    toStringBtn.addEventListener('click', () => {
+        alert(linkedlist.toString());
+    });
     removeElementAtBtn.addEventListener('click',()=>{
         const element= prompt('Enter element to remove from linkedlist');
         const removedE1=linkedlist.remove(element);
@@ -34,6 +62,20 @@ const initiateHandlers=(linkedlist,renderLinkedList)=>{
           } else {
             alert('Element not found');
           }
+    });
+    sizeBtn.addEventListener('click', () => {
+        alert(`The size of the linked list is ${linkedlist.size()}`);
+      });
+    
+    isEmptyBtn.addEventListener('click', () => {
+        alert(`Linked list is${linkedlist.isEmpty() ? '' : ''} empty`);
+    });
+
+
+    clearBtn.addEventListener('click', () => {
+        linkedlist.clear();
+        alert('Linked list cleared');
+        renderLinkedList(linkedlist);
     });
 };
 
