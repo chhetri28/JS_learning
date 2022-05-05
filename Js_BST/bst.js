65309b6bb31bb72e24f057320ce0e9a2474a7cbf
chhetri28 <chettrisourav28@gmail.com>
2022-05-05 20:32:07 +0530

    Class BinarySearchTree Added with insert method

--- Js_BST/bst.js
+++ Js_BST/bst.js
@@ -2,29 +2,72 @@ const COMPARISON = {
     EQUAL: 0,
     SMALLER: -1,
     GREATER: 1,
-  };
-  
+};
+
 const defaultCompareNumberFn = (a, b) => {
     if (Number(a) == Number(b)) {
-      return COMPARISON.EQUAL;
+        return COMPARISON.EQUAL;
     }
-  
+
     return Number(a) < Number(b) ? COMPARISON.SMALLER : COMPARISON.GREATER;
 };
 
 class TreeNode {
     constructor(value, parent) {
-      this.value = value.toString();
-      this.parent = parent || null;
-      this.left = null;
-      this.right = null;
+        this.value = value.toString();
+        this.parent = parent || null;
+        this.left = null;
+        this.right = null;
     }
-  
+
     get isLeaf() {
-      return this.left === null && this.right === null;
+        return this.left === null && this.right === null;
     }
-  
+
     get hasChildren() {
-      return !this.isLeaf;
+        return !this.isLeaf;
+    }
+}
+class BinarySearchTree {
+    oot;
+    compareFn;
+    constructor(compareFn = defaultCompareNumberFn) {
+        this.root = null;
+        this.compareFn = compareFn;
+    }
+    insert(value) {
+        let node = this.root;
+        let insertedNode;
+        if (node === null) {
+            this.root = new TreeNode(value);
+            return this.root;
+        }
+        const nodeInserted = (() => {
+            while (true) {
+                const comparison = this.compareFn(value, node.value);
+                if (comparison === COMPARISON.EQUAL) {
+                    insertedNode = node;
+                    return node;
+                }
+                if (comparison === COMPARISON.SMALLER) {
+                    if (node.left === null) {
+                        insertedNode = new TreeNode(value, node);
+                        node.left = insertedNode;
+                        return true;
+                    }
+                    node = node.left;
+                } else if (comparison === COMPARISON.GREATER) {
+                    if (node.right === null) {
+                        insertedNode = new TreeNode(value, node);
+                        node.right = insertedNode;
+                        return true;
+                    }
+                    node = node.right;
+                }
+            }
+        })();
+        if (nodeInserted) {
+            return insertedNode;
+        }
     }
 }
